58ff1d5cb09fd8f9737747e31710a5bafedff981
manasakalaga <sreemanasa.2000@gmail.com>
2020-09-03 22:55:19 +0530

    added adapters

--- wakeupkoala/bot.py
+++ wakeupkoala/bot.py
@@ -3,39 +3,46 @@ import os
 import time
 import discord
 from datetime import datetime as dt
-import schedule
 from dotenv import load_dotenv
 import requests
 import json
 import hashlib
+import pytz
+from requests.adapters import HTTPAdapter
 
+
+IST = pytz.timezone('Asia/Kolkata')
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 
 def get_quote():
+    s = requests.Session()
     url="https://quotes.rest/qod?language=en"
+    s.mount(url, HTTPAdapter(max_retries=5))
     page = requests.get(url)    
     content = page.content.decode("utf-8")
     new_content = json.loads(content)
     quote = new_content["contents"]["quotes"][0]["quote"]
     return quote
 
-
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
     print(f'{client.user} is connected to the following guild:')
     while True:
-        naive_dt = dt.now()
+        naive_dt = dt.now(IST)
         current_time = naive_dt.strftime("%H:%M:%S")
-        print
-        if current_time == "00:10:00":
-            message = get_quote()
-            channel = client.get_channel(740299223337009172)
-            await channel.send(message)
-            time.sleep(1)
+        if current_time == "22:07:00":
+            try:
+                message = get_quote()
+                channel = client.get_channel(740299223337009172)
+                await channel.send(message)
+                time.sleep(1)
+            except:
+                channel = client.get_channel(740299223337009172)
+                await channel.send("API did not respond!")
 
 
 client.run(TOKEN)
